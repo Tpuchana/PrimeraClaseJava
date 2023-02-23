1be65b6758c7eca12d89558029ce8a61be8717f4
Tomás Puchana <109983088+Tpuchana@users.noreply.github.com>
2023-02-23 15:25:11 -0500

    1

--- PrimeraClaseJava/src/Auto.java
+++ PrimeraClaseJava/src/Auto.java
@@ -16,7 +16,102 @@ public class Auto {
     float km;
     float cilindraje;
     float precio;
+    boolean autoencendido;
+    double velocidad;
+
+
+
+//Metodos
+        //Constructor
+    public Auto(boolean autoencendido, String placa, int modelo, String marca){
+        this.placa = placa;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.autoencendido = false;
+        this.velocidad = 0;
+        
+        
+                
+
 }
+
+ 
+
+   
+    
+    //infoAuto: devuelve la información del objeto.
+    
+    public void infoAuto(){
+        System.out.println("""
+                           
+                Datos Auto;
+                + "Placa: """ + this.placa + "\n"
+                + "Color: " + this.color + "\n"
+                + "Modelo: " + this.modelo + "\n"
+                + "Marca: " + this.marca + "\n"
+                + "Kilometraje: " + this.km + "\n"
+                + "Cilindraje: " + this.cilindraje + "\n"
+                + "Precio: " + this.precio + "\n"
+        );
+    }
+    public void encenderAuto (){
+    if (this.autoencendido == true){
+        System.out.println("El auto ya está encendido");
+    }else{
+        this.autoencendido=true;
+        System.out.println("Encendiendo el auto");
+    }
+    }
+    
+    
+        
+        
+    public void apagarAuto(){
+    if (this.autoencendido == true){
+        this.autoencendido = false;
+        System.out.println("Apagando el auto");
+        this.velocidad = 0;
+    }else{
+        this.autoencendido=false;
+        System.out.println("El auto ya está apagado");
+    }
+    }
+
+    public void acelerarAuto(int velocidad){
+        if (this.autoencendido == true){
+            this.velocidad = this.velocidad + velocidad;
+        }else{
+            System.out.println("Auto apagado, por favor enciéndalo");
+        }
+    }
+    public void frenarAuto(int velocidad){
+        if (this.velocidad>0 && this.autoencendido == true){
+            this.velocidad = this.velocidad-velocidad;
+            System.out.println("deteniendo el auto");
+            
+        }else if(this.velocidad==0)
+            System.out.println("El auto ya está detenido");
+            
+        if (this.velocidad<0){
+            this.velocidad = 0;
+        }
+            
+    }
+    
+    
+    
+            
+                
+               
+       
+        
+        
+   
+        
+        
+}
+    
+    
 
 
 
--- PrimeraClaseJava/src/Principal.java
+++ PrimeraClaseJava/src/Principal.java
@@ -13,69 +13,75 @@ public class Principal {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//        Auto miAuto = new Auto();
-//        
-//        //llamado de atributos
-//            //Asignación de valores
-//        miAuto.placa = "ABC123";
-//        miAuto.color = "Azul";
-//        miAuto.modelo = 2023;
-//        miAuto.marca = "BMW";
-//        miAuto.km = 5230.8f;
-//        miAuto.cilindraje = 2500f;
-//        miAuto.precio = 85000000f;
-//        System.out.println("Datos Auto\n+"
-//                + "Placa: " + miAuto.placa + "\n"
-//                + "Color: " + miAuto.color + "\n"
-//                + "Modelo: " + miAuto.modelo + "\n"
-//                + "Marca: " + miAuto.marca + "\n"
-//                + "Kilometraje: " + miAuto.km + "\n"
-//                + "Cilindraje: " + miAuto.cilindraje + "\n"
-//                + "Precio: " + miAuto.precio + "\n"
-//        );
-//                
-          Gato miGato = new Gato();
-          Perro miPerro = new Perro();
-          
-          miGato.color = "Negro";
-          miGato.edad = 7;
-          miGato.nombre = "Luna";
-          miGato.pelaje = "Liso";
-          miGato.sexo = "Femenino";
-          miGato.tamaño = "Pequeño";
-          miGato.raza = "Bombay";
-          miPerro.color = "Gris";
-          miPerro.edad = 7;
-          miPerro.nombre = "Pepa";
-          miPerro.pelaje = "Crepo";
-          miPerro.raza = "Caniche";
-          miPerro.sexo = "Femenino";
-          miPerro.tamaño = "Mediano";
-          
-          System.out.println("Datos Perro\n"
-                + "Raza: " + miPerro.raza + "\n"
-                + "Color: " + miPerro.color + "\n"
-                + "Edad: " + miPerro.edad + "\n"
-                + "Pelaje: " + miPerro.pelaje + "\n"
-                + "Sexo: " + miPerro.sexo + "\n"
-                + "Tamaño: " + miPerro.tamaño + "\n"
-                + "Nombre: " + miPerro.nombre + "\n"
-                  
-          );   
-          
-           System.out.println("Datos Gato\n"
-                + "Raza: " + miGato.raza + "\n"
-                + "Color: " + miGato.color + "\n"
-                + "Edad: " + miGato.edad + "\n"
-                + "Pelaje: " + miGato.pelaje + "\n"
-                + "Sexo: " + miGato.sexo + "\n"
-                + "Tamaño: " + miGato.tamaño + "\n"
-                + "Nombre: " + miGato.nombre + "\n"
-                  
-          );      
-                  
-                  
-                
-    }
+        Auto miAuto = new Auto(true, "ABC123", 2023, "BMW");
+        
+        //llamado de atributos
+            //Asignación de valores
+        miAuto.color = "Azul";        
+        miAuto.km = 5230.8f;
+        miAuto.cilindraje = 2500f;
+        miAuto.precio = 85000000f;
+        
+        miAuto.infoAuto();
+        miAuto.encenderAuto();
+        System.out.println("Velocidad de miAuto es: "+ miAuto.velocidad);
+        miAuto.acelerarAuto(5);
+        System.out.println("Velocidad de miAuto es: "+ miAuto.velocidad);
+        miAuto.acelerarAuto(30);
+        System.out.println("Velocidad de miAuto es: "+ miAuto.velocidad);
+        miAuto.frenarAuto(50);
+        System.out.println("Velocidad de miAuto es: "+ miAuto.velocidad);
+        miAuto.apagarAuto();
+        System.out.println("Velocidad de miAuto es: "+ miAuto.velocidad);
+        
+            
+    }        
     
 }
+        
+    
+//          Gato miGato = new Gato();
+//          Perro miPerro = new Perro();
+//          
+//          miGato.color = "Negro";
+//          miGato.edad = 7;
+//          miGato.nombre = "Luna";
+//          miGato.pelaje = "Liso";
+//          miGato.sexo = "Femenino";
+//          miGato.tamaño = "Pequeño";
+//          miGato.raza = "Bombay";
+//          miPerro.color = "Gris";
+//          miPerro.edad = 7;
+//          miPerro.nombre = "Pepa";
+//          miPerro.pelaje = "Crepo";
+//          miPerro.raza = "Caniche";
+//          miPerro.sexo = "Femenino";
+//          miPerro.tamaño = "Mediano";
+//          
+//          System.out.println("Datos Perro\n"
+//                + "Raza: " + miPerro.raza + "\n"
+//                + "Color: " + miPerro.color + "\n"
+//                + "Edad: " + miPerro.edad + "\n"
+//                + "Pelaje: " + miPerro.pelaje + "\n"
+//                + "Sexo: " + miPerro.sexo + "\n"
+//                + "Tamaño: " + miPerro.tamaño + "\n"
+//                + "Nombre: " + miPerro.nombre + "\n"
+//                  
+//          );   
+//          
+//           System.out.println("Datos Gato\n"
+//                + "Raza: " + miGato.raza + "\n"
+//                + "Color: " + miGato.color + "\n"
+//                + "Edad: " + miGato.edad + "\n"
+//                + "Pelaje: " + miGato.pelaje + "\n"
+//                + "Sexo: " + miGato.sexo + "\n"
+//                + "Tamaño: " + miGato.tamaño + "\n"
+//                + "Nombre: " + miGato.nombre + "\n"
+//                  
+//          );      
+                  
+                 
+                             
+    
+    
+
